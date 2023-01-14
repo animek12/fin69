94c316f0c8cb75a69ec5f83533f040fc9914cfcb
animek12 <111511065+animek12@users.noreply.github.com>
2023-01-14 19:10:55 +0800

    Update help.js

--- command/help.js
+++ command/help.js
@@ -518,4 +518,4 @@ exports.donate = (pushname, ownerNumber) => {
 wa.me/${ownerNumber} (Owner)`
 }
 
-exports.sewanya = `Untuk List Sewa ubah sendiri di folder command, file help.js 👌 baris ke 521`
+exports.sewanya = `NOT FOUND`
